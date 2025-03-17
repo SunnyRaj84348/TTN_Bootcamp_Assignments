b90f79cd41ffde25ac964186b9afd79ff633d5f3
SunnyRaj84348 <sunny.raj@tothenew.com>
2025-03-17 09:43:43 +0000

    Complete 'Add cart' test

--- pages/ItemPage.js
+++ pages/ItemPage.js
@@ -1,9 +1,14 @@
+import { log } from "console"
+
 class ItemPage {
-    constructor(page) {
+    constructor(page, context) {
         this.page = page
+        this.context = context
 
         this.acSection = page.locator("//a[@aria-label='ACs']")
         this.productSections = page.locator("//div[@role='listitem'][position() <= 2]")
+
+        this.productTitles = []
     }
 
     async goto() {
@@ -16,7 +21,22 @@ class ItemPage {
 
         for (const productSection of await this.productSections.all()) {
             const productLink = productSection.locator("//a")
-            await productLink.first().click()
+
+            const [newPage] = await Promise.all([
+                this.context.waitForEvent('page'),
+                productLink.first().click()
+            ])
+
+            await newPage.bringToFront()
+
+            const productTitle = newPage.locator("//span[@id='productTitle']")
+            this.productTitles.push(await productTitle.textContent())
+
+            const addCart = newPage.locator("//input[@id='add-to-cart-button']").nth(1)
+            await addCart.click()
+
+            await newPage.close()
+            await this.page.bringToFront()
         }
     }
 }
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -2,8 +2,8 @@
 import { test } from '@playwright/test';
 import ItemPage from '../pages/ItemPage';
 
-test('test', async ({ page }) => {
-    const itemPage = new ItemPage(page)
+test('test', async ({ page, context }) => {
+    const itemPage = new ItemPage(page, context)
     await itemPage.goto()
     await itemPage.addToCart()
 });
